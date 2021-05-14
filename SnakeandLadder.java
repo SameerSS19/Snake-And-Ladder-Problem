c02dc79255d51bafa11522decdbc9c953274f8d8
SameerSS19 <sk3090019@gmail.com>
2021-05-14 17:27:33 +0530

    The player then checks for a option

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,12 +1,31 @@
 package com.snakeandladder;
 
 public class SnakeandLadder {
-	public static final int startPosition = 0;
-	public static int dice = (int) ((Math.random() * 10) % 6 + 1);
-	
+	 
 		public static void main(String[] args) {
+			int start_position_firstplayer = 0;
+			int dice = 0;
+			int option = 0;
+			
 			System.out.println("Welcome to The Snake and Ladder Game ");
-			System.out.println(dice);
+			
+			dice = (int) ((Math.random() * 10) % 6 + 1);
+			System.out.println("Dice value:"+ dice);
+			
+			option = (int) ((Math.random() * 10) % 3 + 1);
+			System.out.println("Dice value:"+option);
+			switch(option) 
+			{
+				case 0:
+					System.out.println("No Play");
+					break;
+				case 1:
+					start_position_firstplayer = start_position_firstplayer + dice;
+					break;
+				case 2:
+					start_position_firstplayer = start_position_firstplayer - dice;
+					break;
+			}
 	}
 
 }
