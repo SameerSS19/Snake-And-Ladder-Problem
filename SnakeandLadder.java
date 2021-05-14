c2cd3f70cdee17b341c20c428308575056f2503f
SameerSS19 <sk3090019@gmail.com>
2021-05-14 17:09:30 +0530

    Single player at start position 0

---
+++ SnakeandLadder.java
@@ -0,0 +1,10 @@
+package com.snakeandladder;
+
+public class SnakeandLadder {
+	public static final int startPosition = 0;
+	public static void main(String[] args) {
+	System.out.println("Welcome to The Snake and Ladder Game ");
+	System.out.println("Start position is: "  + startPosition);
+	}
+
+}
